935b741a107dc61785d6d07a532b55c4c919485e
Yosef Arbiv <arbiv@cisco.com>
2022-07-24 18:06:15 -0500

    added metrics

--- server.py
+++ server.py
@@ -17,10 +17,30 @@ from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
     OTLPSpanExporter
 )
 
-provider = TracerProvider(resource=Resource.create({SERVICE_NAME: "yosef-workshop-test"}))
-otlpExporter = OTLPSpanExporter(endpoint="http://colle-loadb-19grjer2k6vx8-58b0be4d87e48569.elb.us-east-1.amazonaws.com:4317")
+provider = TracerProvider(resource=Resource.create({SERVICE_NAME: "yosef-workshop-man"}))
+otlpExporter = OTLPSpanExporter(endpoint="http://3.210.98.245:4317")
 processor = BatchSpanProcessor(otlpExporter)
 provider.add_span_processor(processor)
+
+from opentelemetry import metrics
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+from opentelemetry.sdk.metrics.export import (
+    ConsoleMetricExporter,
+    PeriodicExportingMetricReader,
+)
+import requests
+
+
+metric_reader = PeriodicExportingMetricReader(ConsoleMetricExporter(), export_interval_millis=30*1000)
+meter_provider = MeterProvider(metric_readers=[metric_reader])
+
+# Sets the global default meter provider
+metrics.set_meter_provider(meter_provider)
+
+# Creates a meter from the global meter provider
+meter = metrics.get_meter(__name__)
+
 
 # Sets the global default tracer provider
 trace.set_tracer_provider(provider)
@@ -43,9 +63,11 @@ def root():
 @app.route("/fibInternal")
 def fibHandler():
   value = int(request.args.get('i'))
+  current_span = trace.get_current_span()
+  current_span.set_attribute("parameter", value)
   
   returnValue = 0
-  if value == 1:
+  if value <= 1:
     returnValue = 0
   elif value == 2:
     returnValue = 1
@@ -53,8 +75,13 @@ def fibHandler():
     minusOnePayload = {'i': value - 1}
     minusTwoPayload = {'i': value - 2 }
     
-    respOne = requests.get('http://127.0.0.1:5000/fibInternal', minusOnePayload)
-    respTwo = requests.get('http://127.0.0.1:5000/fibInternal', minusTwoPayload)
+    with tracer.start_as_current_span("getMinusOne") as span:
+      span.set_attribute("payloadValue", value - 1)
+      respOne = requests.get('http://127.0.0.1:5000/fibInternal', minusOnePayload)
+
+    with tracer.start_as_current_span("getMinusTwo") as span:
+      span.set_attribute("payloadValue", value - 2)
+      respTwo = requests.get('http://127.0.0.1:5000/fibInternal', minusTwoPayload)
     
     returnValue = int(respOne.content) + int(respTwo.content)
     
